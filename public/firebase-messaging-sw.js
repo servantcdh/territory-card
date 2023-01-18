c5de40b9e3ae2b7c575a30763ece9d08b933bc70
DonghoChoi <servantcdh@naver.com>
2023-01-18 10:47:05 +0900

    Fix: "Fix push twice receive"

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,18 +18,6 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
-
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "./android-chrome-512x512.png",
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
 self.addEventListener("notificationclick", function (event) {
   event.notification.close();
   event.waitUntil(
